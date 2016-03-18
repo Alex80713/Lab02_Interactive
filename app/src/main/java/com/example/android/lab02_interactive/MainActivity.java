d0dd17d14ab78dc08f4a0f753b6db9d10ab457e7
aa1991713 <a29763846@gmail.com>
2016-03-18 09:44:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/lab02_interactive/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.android.lab02_interactive;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+    private int number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        display(2);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(String.valueOf(number));
+
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        int price = 5;
+        int total = price * number;
+        String myString = NumberFormat.getCurrencyInstance().format(total);
+        priceTextView.setText(myString);
+
+    }
+
+    public void increment(View view) {
+        int quantity = getQuantity();
+        display(++quantity);
+    }
+
+    private int getQuantity() {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        String quantityString = quantityTextView.getText().toString();
+        return Integer.parseInt(quantityString);
+    }
+
+    public void decrement(View view) {
+        int quantity = getQuantity();
+        if (quantity > 0) {
+            display(--quantity);
+        }
+    }
+}
