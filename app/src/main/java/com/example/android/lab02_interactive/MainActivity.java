ff7b132c76beff436a7d4b747001aea0aa29d8c3
aa1991713 <a29763846@gmail.com>
2016-03-21 10:44:40 +0800

    checkbox

--- app/src/main/java/com/example/android/lab02_interactive/MainActivity.java
+++ app/src/main/java/com/example/android/lab02_interactive/MainActivity.java
@@ -3,44 +3,38 @@ package com.example.android.lab02_interactive;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
-
 public class MainActivity extends AppCompatActivity {
-    private final String mNT$ = "NT$";
-    private int number;
+
+    private String mName = "鳴人";
     private int mQuantity = 0;
-    private int mPrice = 5;
-    private StringBuilder mTotalPriceMessage = new StringBuilder(mNT$);
+    private int mPrice = 50;
+    private StringBuilder mPriceMessage = new StringBuilder("NT" + mPrice);
+    private StringBuilder mQuantityMessage = new StringBuilder(mQuantity);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayPriceMessage();
     }
 
     public void submitOrder(View view) {
-        displaytotalPrice();
-    }
-
-
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(String.valueOf(number));
-
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        int price = 5;
-        int total = price * number;
-        String myString = NumberFormat.getCurrencyInstance().format(total);
-        priceTextView.setText(myString);
+        clearPriceMessageString();
+        concatPriceMessageString();
+        displayPriceMessage();
 
     }
+
+
 
     public void increment(View view) {
         ++mQuantity;
-        displayQuantity();
-        resetTotolPrice();
+        displayQuantityMessage();
+        resetPriceMessageString();
+        displayPriceMessage();
     }
 
     private int getQuantity() {
@@ -50,34 +44,70 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void decrement(View view) {
-        if (mQuantity > 0) {
-            --mQuantity;
-            displayQuantity();
-            resetTotolPrice();
+        if (mQuantity == 0) {
+            return;
+        }
+        --mQuantity;
+        displayQuantityMessage();
+        resetPriceMessageString();
+        displayPriceMessage();
 
+    }
+
+    private void displayPriceMessage() {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(mPriceMessage);
+    }
+
+
+
+    private void displayQuantityMessage() {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        int start = 0;
+        int end = mQuantityMessage.length();
+        mQuantityMessage.delete(start, end).append(mQuantity);
+        quantityTextView.setText(mQuantityMessage);
+    }
+
+    private void clearPriceMessageString() {
+        int start = 0;
+        int end = mPriceMessage.length();
+        mPriceMessage.delete(start, end);
+    }
+
+    private void concatPriceMessageString() {
+        CheckBox checkBox = (CheckBox) findViewById(R.id.toppings_checkbox);
+        mPriceMessage.append("Name: ")
+                .append(mName)
+                .append("\n")
+                .append("臭豆腐")
+                .append("\n")
+                .append("加泡菜 ? ")
+                .append(checkBox.isChecked())
+                .append("\n");
+        if (mQuantity == 0) {
+            mPriceMessage.append("Free");
+        } else {
+            mPriceMessage.append("Quantity: ")
+                    .append(mQuantity)
+                    .append("\n")
+                    .append("Total: ")
+                    .append("NT$")
+                    .append(mPrice * mQuantity)
+                    .append("\n")
+                    .append("Thank you!")
+                    .append("\n");
         }
     }
 
-    private void resetTotolPrice() {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText("");
+    public void clickToppings(View view) {
+        resetPriceMessageString();
+        displayPriceMessage();
     }
 
-    private void displayQuantity() {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(String.valueOf(mQuantity));
+    private void resetPriceMessageString() {
+        clearPriceMessageString();
+        displayPriceMessage();
     }
 
-    private void displaytotalPrice() {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        int total = mPrice * mQuantity;
-        int startIndex = mNT$.length();
-        int endIndex = mTotalPriceMessage.length();
-        mTotalPriceMessage.delete(startIndex, endIndex).append(total)
-                .append(mQuantity == 0 ? "\nFree" : "\nThank you!");
-        priceTextView.setText(mTotalPriceMessage);
-        String myString = NumberFormat.getCurrencyInstance().format(total);
-        String message = myString + (mQuantity == 0 ? "\nFree" : "\nThank you!");
-        priceTextView.setText(message);
-    }
 }
